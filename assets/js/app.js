68af396572269b6f04801e6427f6da64e41e7351
fabriceLatri <fabrice.latri@hotmail.fr>
2021-02-17 19:42:38 +0100

    Lesson CSS & SCSS done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,5 @@
 import  { log } from './log';
-// import $ from 'jquery';
-
-let a = "Salut les gens";
-
-log(a);
-
-console.log('salut');
-
-document.getElementById('button').addEventListener('click', () => {
-  import('jquery').then($ => {
-    
-    $('body').css('backgroundColor', '#FF0000');
-    debugger;
-
-  })
-})
+import css from '../styles/app.scss';
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,27 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const dev = process.env.NODE_ENV === 'dev';
 
+let cssLoaders = [
+  'style-loader', 
+  { loader: 'css-loader', options: { importLoaders: 1 } }
+]
+
+if (!dev) {
+  cssLoaders.push(
+    { loader: 'postcss-loader',
+      options: {
+        plugins: () => [
+          require('autoprefixer')({
+            browsers: ['last 2 versions', 'ie > 8']
+          })
+        ]
+      }
+    }
+  )
+}
+  
+
+
 let config = {
   entry: './assets/js/app.js',
   
@@ -21,6 +42,14 @@ let config = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: cssLoaders
+      },
+      {
+        test: /\.scss$/,
+        use: [...cssLoaders, 'sass-loader']
       }
     ]
   },
